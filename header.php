6406c863553d3e4e0c12a5bea4a3dc9f66c07f6e
pecodrive <pecodrive@gmail.co>
2016-03-27 12:29:21 +0900

    ad traking code

--- header.php
+++ header.php
@@ -10,6 +10,20 @@
         <link rel="SHORTCUT ICON" href="<?php echo get_template_directory_uri();?>/img/favicon.png">
     </head>
     <body>
+<?php if ( !is_user_logged_in() ){
+echo <<<aaa
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-36078315-9', 'auto');
+          ga('send', 'pageview');
+        </script>
+aaa;
+}
+?>
         <div class="container-fluid">
             <div class="main col-xs-12 col-sm-9 col-md-9 col-lg-9">
                 <header class="text-center">
