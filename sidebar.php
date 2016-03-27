6ec1c4794b89ecabd39c076c890b0c066a356868
pecodrive <pecodrive@gmail.co>
2016-03-27 16:33:05 +0900

    ad

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,7 @@
 <div class="sidebar col-xs-12 col-sm-3 col-md-3 col-lg-3">
     <?php get_template_part("authordiv"); ?>
     <?php get_template_part("newarticle"); ?>
+    <?php get_template_part("sidebar_ad"); ?>
     <?php get_template_part("categorylist"); ?>
     <?php get_template_part("archivelist"); ?>
 </div>
---
+++ sidebar_add.php
@@ -0,0 +1,4 @@
+<div>
+<iframe src="http://rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&nou=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=pecodrive-22&o=9&p=8&l=as1&m=amazon&f=ifr&ref=tf_til&asins=B015IZT854" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+</div>
+
