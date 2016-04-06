4b4245c749390012dfe07c3d823209166d7ff5cf
pecodrive <pecodrive@gmail.co>
2016-04-06 14:24:08 +0900

    allarticle

--- allarticle.php
+++ allarticle.php
@@ -20,10 +20,10 @@
     <?php
         $big = 999999999; 
         echo paginate_links( array(
-            'base' => str_replace( $big, '%#%', esc_url( $allarticle->get_pagenum_link( $big ) ) ),
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
             'show_all'=>true,
-            'format' => 'page/%#%',
-            'current' => max( 1, $allarticle->get_query_var('paged') ),
+            'format' => '/page/%#%',
+            'current' => max( 1, get_query_var('paged') ),
             'total' => $allarticle->max_num_pages
         ) );
     ?>
