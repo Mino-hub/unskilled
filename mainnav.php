4eb37880b569bb7386748a55606ee9b7a49c290c
pecodrive <pecodrive@gmail.co>
2016-04-06 13:42:06 +0900

    allarticle

--- mainnav.php
+++ mainnav.php
@@ -1,3 +1,4 @@
 <li><a href="<?php echo bloginfo("url") ?>">ホーム</a></li>
-<li><a href="<?php echo bloginfo("url") . "/?page_id=2391" ?>">全記事</a></li>
+<li><a href="<?php echo bloginfo("url") . "/?page_id=670" ?>">全記事</a></li>
+<li><a href="<?php echo bloginfo("url") . "/%ef%bc%a0mino%e3%81%ae%e3%83%97%e3%83%ad%e3%82%b0%e3%83%a9%e3%83%9f%e3%83%b3%e3%82%b0%e5%ad%a6%e7%bf%92%e3%81%ae%e5%a4%89%e9%81%b7%ef%bc%88%e3%81%a7%e3%81%8d%e3%82%8b%e3%81%a0%e3%81%91%e6%99%82" ?>">@MINOのプログラミング学習記</a></li>
 <li><a href="<?php echo bloginfo("url") . "/このサイトについて" ?>">このサイトについて</a></li>
