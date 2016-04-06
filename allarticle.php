b98c1165e64d8e416443b882c307e076ade07451
pecodrive <pecodrive@gmail.co>
2016-04-06 14:07:18 +0900

    allarticle

--- allarticle.php
+++ allarticle.php
@@ -18,7 +18,6 @@
 <?php if ($allarticle->have_posts()) : ?>    
 <div class="pageNation">
     <?php
-        $allarticle;
         $big = 999999999; 
         echo paginate_links( array(
             'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
