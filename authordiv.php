d1d2ee57f817090286a91c6ff98328c8b97fbab2
pecodrive <pecodrive@gmail.co>
2016-04-07 00:55:54 +0900

    imagefix

--- authordiv.php
+++ authordiv.php
@@ -4,7 +4,7 @@
     </div>
     <div class="authorSpace text-center">
         <div class="authorName">@MINO</div>
-        <img src="<?php echo get_template_directory_uri();?>/img/char.png" width=100 height=100>
+        <img src="<?php echo get_template_directory_uri();?>/img/char.jpg" width=100 height=100>
         <p>長年働いていた会社をリストラされてしまったおっさん。なにを思ったか未経験のプログラミングで再起を図ろうとする。</p>
         <p>プログラミングを勉強していく上で苦しんだこと、ハマったことを中心に記事を書きます。</p>
     </div>
